795b66e9752baf0bee5fdbbd3d2af1392393f2d9
JulenHdez <julenhernandezdiaz@gmail.com>
2024-04-23 18:04:42 +0200

    second commit

--- admin/productos/insertar.php
+++ admin/productos/insertar.php
@@ -2,76 +2,166 @@
 
 require '../database.php';
 
-$db=conectarDB();
+$db = conectarDB();
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+
+//Array con mensajes de errores
+
+
+
+
+//Obtener categorias y proveedores para usar en el formulario
+$sqlCategorias = "SELECT idcategoria, nombre_categoria FROM categorias ORDER BY nombre_categoria ASC";
+$resultadoCategorias = mysqli_query($db, $sqlCategorias);
+$categorias = mysqli_fetch_all($resultadoCategorias, MYSQLI_ASSOC);
+
+$sqlProveedores = "SELECT idproveedor, nombre_proveedor FROM proveedores ORDER BY nombre_proveedor ASC";
+$resultadoProveedores = mysqli_query($db, $sqlProveedores);
+$proveedores = mysqli_fetch_all($resultadoProveedores, MYSQLI_ASSOC);
+
+
+
+$errores = [];
+
+//EJECUTAR CONSULTA
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+
+
 
     $nombre_producto = $_POST['nombre_producto'];
     $descripcion = $_POST['descripcion'];
     $precio = $_POST['precio'];
     $stock_disponible = $_POST['stock_disponible'];
     $categorias_idcategorias = $_POST['categorias_idcategorias'];
-    $proveedor_idproveedor = $_POST['proveedor_idproveedor'];
-    $sql = "INSERT INTO productos (nombre_producto, descripcion, precio, stock_disponible, categorias_idcategorias, proveedor_idproveedor)
+    $proveedores_idproveedor = $_POST['proveedores_idproveedor'];
 
-    VALUES ('$nombre_producto', '$descripcion', '$precio', '$stock_disponible', '$categorias_idcategorias', '$proveedor_idproveedor')";
+    if(!$nombre_producto) {
+        $errores[] = "Debes añadir el nombre del articulo";
+    }
 
-    $resultado = mysqli_query($db, $sql);
+    if(strlen($descripcion) < 30) {
+        $errores[] = "Debes añadir una descripción de minimo 50 caractertes";
+    }
+
+    if(!$stock_disponible) {
+        $errores[] = "Falta el Stock disponible";
+    }
+
+    if(!$precio) {
+        $errores[] = "El precio es obligatorio";
+    }
+
+    // Revisar que el arreglo de errores este vacio y ejecutar consulta si lo esta
+
+    if (empty($errores)) {
+        $sql = "INSERT INTO productos (nombre_producto, descripcion, precio, stock_disponible, categorias_idcategorias, proveedores_idproveedor)
+
+        VALUES ('$nombre_producto', '$descripcion', '$precio', '$stock_disponible', '$categorias_idcategorias', '$proveedores_idproveedor')";
+    
+        $resultado = mysqli_query($db, $sql);
+    
+        if ($resultado) {
+            header('Location: /admin/productos/correcto.php');
+        }
+    }
+     
+
 }
 
 ?>
 
-<main class="contenedor">
-<h1>Crear</h1>
+<!DOCTYPE html>
+<html lang="en">
 
-<a href="/admin" class="boton boton-verde">Volver</a>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Magnolia Coffee</title>
+    <link rel="stylesheet" href="/build/css/app.css">
+</head>
+
+<header>
 
 
-<form action="" class="formulario" method="POST" action="/admin/propiedades/crear.php">
-    <fieldset>
-        <legend>Información General</legend>
-        <label for="titulo">Titulo:</label>
-        <input type="text" id="titulo" name="titulo" placeholder="Titulo Propiedad">
+</header>
 
-        <label for="precio">Precio:</label>
-        <input type="number" id="precio" name="precio" placeholder="Precio Propiedad">
-
-        <label for="imagen">Imagen:</label>
-        <input type="file" id="imagen" accept="image/jpeg, image/png">
-
-        <label for="descripcion">Descripcion:</label>
-        <textarea id="descripcion" name="descripcion"></textarea>
-    </fieldset>
-
-    <fieldset>
-        <legend>Información de la propiedad</legend>
-
-        <label for="habitaciones">Habitaciones:</label>
-        <input type="number" id="habitaciones" name="habitaciones" placeholder="Ej:3" min="1" max="9">
-
-        <label for="wc">Baños:</label>
-        <input type="number" id="wc" name="wc" placeholder="Ej:3" min="1" max="9">
-
+<body>
+    <main class="contenedor seccion">
+        <div class="banner">
+            <h1 class="banner-form">Actualizar Articulos</h1>
+        </div>
         
-        <label for="estacionamiento">Estacionamiento:</label>
-        <input type="number" id="estacionamiento" name="estacionamiento" placeholder="Ej:3" min="1" max="9">
+        <input type="submit" value="Volver" href="" class="boton boton-verde">
 
-    </fieldset>
+        <?php foreach($errores as $error): ?>
+            <div class="alerta error">
+            <?php echo $error; ?>
+            </div>
+        <?php endforeach; ?>
+        
 
-    <fieldset>
-        <legend>Vendedor</legend>
+        <form class="formulario" method="POST" action="/admin/productos/insertar.php">
 
-        <select name="vendedorId" id="">
-            <option value="1">Juan</option>
-            <option value="2">Julen</option>
+            <fieldset>
 
-        </select>
-    </fieldset>
+                <legend>Descripcion producto</legend>
 
-    <input type="submit" value="Crear Propiedad" class="boton boton-verde">
+                <label for="proveedor">Proveedor:</label>
+                <select name="proveedores_idproveedor" id="proveedor">
+            
+                    <?php foreach ($proveedores as $proveedor) : ?>
+                        <option value="<?= $proveedor['idproveedor'] ?>"><?= $proveedor['nombre_proveedor'] ?></option>
+                    <?php endforeach ?>
+
+                </select>
+
+                <label for="nombre_producto">Nombre:</label>
+                <input type="text" id="nombre_producto" name="nombre_producto" placeholder="Marca producto">
+
+                <br>
+
+                <label for="descripcion">Descripcion:</label>
+                <textarea id="descripcion" name="descripcion"></textarea>
+
+                <br>
+
+                <label for="categoria">Categoria:</label>
+                <select name="categorias_idcategorias" id="categoria">
+                    <?php foreach ($categorias as $categoria) : ?>
+                        <option value="<?= $categoria['idcategoria'] ?>"><?= $categoria['nombre_categoria'] ?></option>
+                    <?php endforeach ?>
+                </select>
+
+
+                <br>
+
+                <label for="precio">Precio:</label>
+                <input type="number" id="precio" name="precio" placeholder="Precio Producto">
+
+                <br>
+
+                <label for="imagen">Imagen:</label>
+                <input type="file" id="imagen" accept="image/jpeg, image/png">
+
+                <br>
+
+                <label for="stock_disponible">Stock Disponible</label>
+                <input type="number" id="stock_disponible" name="stock_disponible" placeholder="Ej:3" min="1">
+
+            </fieldset>
 
 
 
-</form>
+            <input type="submit" value="Enviar" class="boton boton-verde">
+            <input type="submit" value="Cancelar" class="boton boton-rojo">
 
-</main>
+
+
+        </form>
+
+    </main>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Magnolia Coffee</title>
+    <link rel="stylesheet" href="/build/css/app.css">
+</head>
+<body>
+
+    <header class="header">
+        <div class="contenedor contenido-header">
+            <div class="barra">
+                <a href="/">
+                    <img src="" alt="Logotivo Magnolia Coffee">
+                </a>
+                
+                <nav class="navegacion">
+                    <a href="/">Home</a>
+                    <a href="nosotros.html">Nosotros</a>
+                    <a href="blog.html">Blog</a>
+                    <a href="contacto.php">Contacto</a>
+                    <a href="carrito.html">Carrito</a>
+                </nav>
+
+            </div>
+        </div>
+    </header>
+    <div class="main-banner">
+        <h1>Sabor que conecta mundos</h1>
+    </div>
+ 
+    <div class="sidebar">
+        <a href="">Café en grano</a>
+        <a href="">Cafe Molido</a>
+        <a href="">Cafeteras</a>
+        <a href="">Accesorios</a>
+        <a href="">Capsulas Nespresso</a>
+    </div>
+
+    <main class="contenedor img-index seccion">
+
+
+    <h1>Bienvenido a Magnolia Coffee</h1>
+
+
+
+    </main>
+    
+
+    <footer class="footer seccion">
+
+        <div class="contenedor contenido-footer">
+            <nav class="navegacion">
+                <a href="contacto.php">Contacto</a>
+                <a href="">Instagram</a>
+                <a href="">Twitter</a>
+                <a href="">Facebook</a>
+            </nav>
+        </div>
+
+        <p class="newsletter contenedor">  Subscribete a nuestra newsletter</p>  
+        <p class="copyright">Todos los derechos reservados <?php echo date('Y'); ?> &copy;</p>
+          
+    </footer>
+    
+    <script src="/build/js/bundle.min.js"></script>
+</body>
+</html>
